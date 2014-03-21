e728581886f5d76a5e474c865e332be5c9cf8183
Marcelo Sampaio <marcelo@artlogica.com.br>
2014-03-21 09:36:56 -0300

    creating version 1 - development

---
+++ myFlappyBird/Game.h
@@ -0,0 +1,13 @@
+//
+//  Game.h
+//  myFlappyBird
+//
+//  Created by Marcelo Sampaio on 3/21/14.
+//  Copyright (c) 2014 Marcelo Sampaio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Game : UIViewController
+
+@end
