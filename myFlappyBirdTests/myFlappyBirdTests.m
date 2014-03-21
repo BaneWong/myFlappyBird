32199c6544ae18613fd86d6d49cbffab6835e064
Marcelo Sampaio <marcelo@artlogica.com.br>
2014-03-21 09:02:28 -0300

    Initial Commit

---
+++ myFlappyBirdTests/myFlappyBirdTests.m
@@ -0,0 +1,34 @@
+//
+//  myFlappyBirdTests.m
+//  myFlappyBirdTests
+//
+//  Created by Marcelo Sampaio on 3/21/14.
+//  Copyright (c) 2014 Marcelo Sampaio. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface myFlappyBirdTests : XCTestCase
+
+@end
+
+@implementation myFlappyBirdTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
